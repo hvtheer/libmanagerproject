a7379551b7ea1ea0a6c7cea7bbca6cfc9fdb26ca
The Hoang <89453138+hvtheer@users.noreply.github.com>
2022-07-13 17:57:26 +0700

    add dropdown

---
+++ layout/dropdown.php
@@ -0,0 +1,12 @@
+<div class ="optional">
+    <ul>
+        <li class="dropdown">
+                Hello
+                    <div class="dropdown-content">
+                        <a href="#">Profile</a>
+                        <a href="#">Transactions</a>
+                        <a href="logout.php">Log out</a>
+                    </div>
+        </li>
+    </ul>
+</div>
