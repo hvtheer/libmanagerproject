2294d8828997218d282b95cd5e33e7e813e3e1e1
The Hoang <89453138+hvtheer@users.noreply.github.com>
2022-07-15 23:13:05 +0700

    Delete disconn_andconn directory

--- disconn_andconn/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$Conn = pg_connect("host=localhost port=5432 dbname=libmaneger user=postgres password=125217");
-if (!$Conn) {
-    echo "An error occurred.\n";
-    exit;
-}
-session_start();
-// Query data
-?>
--- disconn_andconn/disconnectdb.php
+++
@@ -1,6 +0,0 @@
-<?php 
-if(!pg_close($Conn)) {
-	print "Failed to close connection to " . pg_host($pgsql_conn) . ": " .
-   pg_last_error($Conn) . "<br/>\n";
-}
-?>
