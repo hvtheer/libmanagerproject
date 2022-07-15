7e4450750e47d09cc2c60632d9a96326089b0815
The Hoang <89453138+hvtheer@users.noreply.github.com>
2022-07-15 23:13:28 +0700

    Delete status.php

--- status.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if(!isset($_SESSION["signinstatus"])){
-        $_SESSION["signupstatus"] = 0;
-	    $_SESSION["signinstatus"] = 0;
-        $_SESSION["typeaccount"] = $_SESSION["accountname"] = $_SESSION["accountpassword"] = "";
-    }
-?>
